1057801a78ce6e3f9e8e7ac2eeac7cdcd0fda72b
ei17ringo <ei17ringo@gmail.com>
2016-10-24 00:40:01 +0800

    init commit

---
+++ sampleSwipeGesture/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  sampleSwipeGesture
+//
+//  Created by Eriko Ichinohe on 2016/02/02.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func swipeView(_ sender: UISwipeGestureRecognizer) {
+        
+        print("右スワイプされた")
+    }
+
+    @IBAction func swipePinkView(_ sender: AnyObject) {
+        
+        print("下スワイプされた")
+    }
+}
+
